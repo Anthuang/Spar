2672720c935dac60144d596c6f0f94b6a686d3e1
Anthony <Anthony@14-144-184.guest.wireless.msu.edu>
2016-02-28 04:07:54 -0500

    made changes

---
+++ form.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form</title>
+    <link rel="stylesheet" type="text/css" href="form.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div id="outer_wrap">
+        <div id="first_display" class="display_wrap">
+            <label for="input_text" id="input_text_label">In 3 words, Thomas Huang is</label>
+            <input id="input_text" type="text"></input><button id="input_submit">enter</button>
+        </div>
+        <div class="display_wrap">
+            <div class="display_text">Wow!</div><!--
+         --><div class="vote_wrap">
+                <div class="vote_up">Up</div>
+                <div class="vote_down">Do</div>
+            </div>
+        </div>
+        <div class="display_wrap">
+            <div class="display_text">Wow!</div><!--
+         --><div class="vote_wrap">
+                <div class="vote_up">Up</div>
+                <div class="vote_down">Do</div>
+            </div>
+        </div>
+        <div class="display_wrap">
+            <div class="display_text">Wow!</div><!--
+         --><div class="vote_wrap">
+                <div class="vote_up">Up</div>
+                <div class="vote_down">Do</div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>The Hate Generator</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div id="intro_wrap">
+        <div id="text_wrap">
+            <div id="static_text">Thomas <br> Huang is</div>
+            <div id="flow_text">a barack sheep<span id="blinker">|</span></div>
+        </div>
+        <a href="form.php"><button id="write_button">write your own</button></a>
+    </div>
+</body>
+</html>
