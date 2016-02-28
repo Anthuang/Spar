a3cc12f9a409ad43966b25d5ec7312ca0dd89140
Anthony <Anthony@14-144-184.guest.wireless.msu.edu>
2016-02-28 02:41:39 -0500

    Added base files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>The Hate Generator</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div id="text_wrap">
+        <div id="static_text">Thomas <br> Huang is</div>
+        <div id="flow_text">a baaaaaaaaaaaaaaaaaaaaaaaaaaaa sheep<span id="blinker">|</span></div>
+    </div>
+</body>
+</html>
