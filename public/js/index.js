5cbc6f829a02b94457f8cd4777ca72ff0d81fc1f
Anthony <Anthony@14-144-184.guest.wireless.msu.edu>
2016-02-28 02:17:55 -0500

    REVISION TIME

--- public/js/index.js
+++
@@ -1,9 +0,0 @@
-var ref = new Firebase("https://sizzling-inferno-4813.firebaseio.com");
-
-ref.authWithOAuthPopup("facebook", function(error, authData) {
-    if (error) {
-        console.log("Login Failed!", error);
-    } else {
-        console.log("Authenticated successfully with payload:", authData);
-    }
-});
